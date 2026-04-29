463010634052152a5f0f2fdbec6f5c427f519d74
aaditi-singhh <aditisingh6841@gmail.com>
2026-04-29 12:32:43 +0530

    complete project done

--- Frontend/src/app/App.jsx
+++ Frontend/src/app/App.jsx
@@ -1,7 +1,7 @@
 import "./App.css"
 import {Editor} from "@monaco-editor/react"
 import { MonacoBinding } from "y-monaco"  //editor se connect ho jati hai toh user jo change karenge toh baki user pe jake broadcast ho jati hai update hote rehti hai..
-import { useRef, useMemo } from "react"  //usememo for creating ydoc
+import { useRef, useMemo, useState , useEffect  } from "react"  //usememo for creating ydoc
 import * as Y from "yjs"
 import { SocketIOProvider} from "y-socket.io"
 
@@ -9,7 +9,12 @@ function App() {
 
 
   const editorRef = useRef(null)
-   
+  const [username, setUsername] = useState(() => {
+    return new URLSearchParams(window.location.search).get("username") || ""  //load karne pe same page mein rehega
+  })
+
+
+  const [users, setUsers] = useState([]) //user store karne ke liye list
 
   const ydoc = useMemo(() => new Y.Doc() , []) 
   //if you have multiple files unn sabko ko ydoc mein store kiya jata hai 
@@ -24,24 +29,119 @@ function App() {
   const handleMount = ( editor ) => {
     editorRef.current = editor
 
+    const monacoBinding = new MonacoBinding(  //editor aur yjs dono ko connect karta hai taki jo editor edit karta hai server ke tthrough dushre user ko broadcast karta hai
+    yText,
+    editorRef.current.getModel(),
+    new Set ([editorRef.current]),
+    //provider.awareness        //konsa user connect hai..jitne bhi user yjs ke help se same editor pe rehte hai sync rehte hai unn sabki details ismein rehti hai 
+  )
+}
+
+    
+ 
+const handleJoin = (e) => {
+  e.preventDefault()
+  setUsername(e.target.username.value)
+  window.history.pushState({}, "" , "?username=" + e.target.username.value) //ye karne se reload hone pe tumhara log in ka url banega taki save rahe sab
+}
+
+
+
+useEffect (() => {
+
+  console.log(username)
+
+
+  if(username){
 
     //socketio rovider user aur server ke bich ka connection provide
   const provider = new SocketIOProvider("http://localhost:3000" , "monaco" ,ydoc,{ //apka editor jo frontend mein hai user aur server ka connection stabilize karne ka kam karte hai
   autoConnect: true,
   })
-    const monacoBinding = new MonacoBinding(  //editor aur yjs dono ko connect karta hai taki jo editor edit karta hai server ke tthrough dushre user ko broadcast karta hai
-    yText,
-    editorRef.current.getModel(),
-    new Set ([editorRef.current]),
-    provider.awareness        //konsa user connect hai..jitne bhi user yjs ke help se same editor pe rehte hai sync rehte hai unn sabki details ismein rehti hai 
-  )
+
+  provider.awareness.setLocalStateField("user" , {username})  //jitne vi user honge unn sabhi ko pehle join karne se username set karna he rehega.. log in ke baad user ka name setlocalstate mein set ho jayega
+
+
+const states = Array.from(provider.awareness.getStates().values())
+
+console.log(states)
+
+
+setUsers(states.filter(state=> state.user && state.user.username).map(state => state.user))
+
+
+
+
+  provider.awareness.on("change" , () => {   //awareness se kitne user connected hai kitne users ja chuke hai sab batata hai on change pe batata hai 
+    
+  const states = Array.from(provider.awareness.getStates().values())
+    setUsers(states.filter (state => state.user && state.user.username).map(state => state.user))
+  
+  })
+
+  function handleBeforeUnload(){
+    provider.awareness.setLocalStateField("user", null)
+
   }
+  
+  window.addEventListener("beforeunload" , handleBeforeUnload)  // before unload--- refresh karne se pehle  ye kam karega..ensures agar koi user refresh ki page unload then yjs se sabhi condition null then return karenge .destroy .disconnect .remove event listen
+   
+    
+
+
+  return () => {
+    
+    provider.disconnect()
+    window.removeEventListener("beforeunload" , handleBeforeUnload)
+  }
+  
+  }
+}, [
  
+  username
+])
+
+
+
+
+
+
+  if(!username){
+    return (
+      <main className=" h-screen w-full bg-gray-950 flex gap-4 p-4 items-center justify-center" >
+        <form 
+        onSubmit = {handleJoin}
+        className = "flex flex-col gap-4">
+          <input
+          type ="text"
+          placeholder ="Enter your username"
+          className="p-2 rounded-lg bg-gray-800 text-white"
+          name ="username"
+        />
+        <button
+        className="p-2 rounded-lg bg-amber-50 text-gray-950 font-bold"
+        >
+          Join
+        </button>
+        </form>
+      </main>
+    )
+  }
 
   return (
     <main
     className="h-screen w-full bg-gray-950 flex gap-4 p-4"> 
-    <aside className = "h-full w-1/4 bg-amber-50 rounded-lg">
+    <aside 
+    className = "h-full w-1/4 bg-amber-50 rounded-lg">
+      <h2 className="text-2xl font-bold p-4 border-b border-gray-300"> Users </h2>
+      <ul className = "p-4">
+        {users.map((user , index) => (
+          <li key = {index} className = "p-2 bg-gray-800 text-white rounded mb-2">
+            {user.username}
+          </li>
+        ))}
+      </ul>
+
     </aside>
     
     <section
